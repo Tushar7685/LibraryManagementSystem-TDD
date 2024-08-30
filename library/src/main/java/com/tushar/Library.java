62979457405f9d6f44c02674563acc3fdf00ac63
Tushar Mandani <136555280+Tushar7685@users.noreply.github.com>
2024-08-31 00:04:23 +0530

    Set up Maven project structure

---
+++ library/src/main/java/com/tushar/Library.java
@@ -0,0 +1,14 @@
+package com.tushar;
+
+/**
+ * Hello world!
+ *
+ */
+public class Library 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
