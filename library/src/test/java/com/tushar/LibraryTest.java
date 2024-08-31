6d341dc9c1ac2e955f637f6cdf415758b019a88c
Tushar Mandani <136555280+Tushar7685@users.noreply.github.com>
2024-08-31 15:22:45 +0530

    [RED] Implemented test case for add book with null value

--- library/src/test/java/com/tushar/LibraryTest.java
+++ library/src/test/java/com/tushar/LibraryTest.java
@@ -1,7 +1,8 @@
 package com.tushar;
 
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class LibraryTest
 {
@@ -10,10 +11,14 @@ public class LibraryTest
     @Test
     public void testAddBook() {
 
-    // test case for add book
+    //1. test case for add book
         Book validBook = new Book("9781612680194", "Rich Dad Poor Dad", "Robert T. Kiyosaki", 1997);
         library.addBook(validBook);
 
+    //2. test case for add book with null value
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> library.addBook(null));
+        assertEquals("Book should not be null", exception.getMessage());
 
-}
+
+    }
 }
