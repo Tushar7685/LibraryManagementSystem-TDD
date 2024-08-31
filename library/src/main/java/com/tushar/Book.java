c64257ea5924e4002153baae6508d5534ac515fd
Tushar Mandani <136555280+Tushar7685@users.noreply.github.com>
2024-08-31 13:26:58 +0530

    [Add] Implemented Book class with attributes

---
+++ library/src/main/java/com/tushar/Book.java
@@ -0,0 +1,15 @@
+package com.tushar;
+
+public class Book {
+    private final String isbn;
+    private final String title;
+    private final String author;
+    private final int publicationYear;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+}
